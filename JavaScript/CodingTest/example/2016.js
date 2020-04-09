5383c2ebc000cb894b91fc10756eefcdc8706088
ussu1112 <ussu1112@naver.com>
2020-04-09 16:50:43 +0900

    postgis geometry error

---
+++ JavaScript/CodingTest/example/2016.js
@@ -0,0 +1,18 @@
+/*
+2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, 
+solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT 입니다. 
+예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 TUE를 반환하세요.
+
+제한 조건
+2016년은 윤년입니다.
+2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+*/
+
+const a = 5
+const b = 24
+day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+answer = [new Date(2016, a - 1, b).getDay()]
+searchDay = new Date(2016,a-1,b).toString().slice(0,3).toUpperCase();                                                                
+console.log(new Date(2016,a-1,b).toString());//Tue May 24 2016 00:00:00 GMT+0900 (대한민국 표준시)
+console.log(searchDay);
+
